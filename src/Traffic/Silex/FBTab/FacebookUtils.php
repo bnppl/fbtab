6911a7f42fc35eb595b74b19217ff2a9a91e6b81
Ben Eppel <ben@trafficdigital.com>
2012-09-06 12:23:57 +0100

    fix liking bug

--- src/Traffic/Silex/FBTab/FacebookUtils.php
+++ src/Traffic/Silex/FBTab/FacebookUtils.php
@@ -19,27 +19,21 @@ class FacebookUtils
     {
         
         $signedRequest = $this->getSignedRequest();
-        if(!$signedRequest)
+        if($signedRequest)
         {
-        if (isset($_REQUEST['signed_request']))
-        {
-            $data = $_REQUEST['signed_request'];
-            $encoded_sig = null;
-            $payload = null;
-            list($encoded_sig, $payload) = explode('.', $data, 2);
-            $sig = base64_decode(strtr($encoded_sig, '-_', '+/'));
-            $data = json_decode(base64_decode(strtr($payload, '-_', '+/'), true));
+ 
+                $data = $signedRequest;
+                $encoded_sig = null;
+                $payload = null;
+                list($encoded_sig, $payload) = explode('.', $data, 2);
+                $sig = base64_decode(strtr($encoded_sig, '-_', '+/'));
+                $data = json_decode(base64_decode(strtr($payload, '-_', '+/'), true));
 
-            $is_fan = isset($data->page->liked)? $data->page->liked : '';
-            return $is_fan;
-        }
+                $is_fan = isset($data->page->liked)? $data->page->liked : '';
+                return $is_fan;
+            
         }
 
-
-        if(isset($signedRequest['page']) && isset($signedRequest['page']['liked']) && $signedRequest['page']['liked'] ==true)
-        {
-        return true;
-        }
         return false;
     }
 }
